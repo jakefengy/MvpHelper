f9a97d6872f0358b91fd48ded962d914db943308
小孩子xm <lvxiaogua@foxmail.com>
2016-06-16 18:10:47 +0800

    1、屎一样的swing

--- src/com/mvp/action/HelloDialogAction.java
+++ src/com/mvp/action/HelloDialogAction.java
@@ -4,7 +4,6 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
-import com.mvp.dialog.MvpDialog;
 
 /**
  */
@@ -13,8 +12,6 @@ public class HelloDialogAction extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent e) {
         Application application = ApplicationManager.getApplication();
-        MvpDialog dialog = application.getComponent(MvpDialog.class);
-        dialog.pack();
-        dialog.setVisible(true);
+
     }
 }
---
+++ src/com/mvp/gui/MvpEditor.java
@@ -0,0 +1,16 @@
+package com.mvp.gui;
+
+import javax.swing.*;
+
+/**
+ */
+public class MvpEditor {
+    private JPanel MvpRoot;
+    private JTextField comSampleTextField;
+    private JRadioButton activityRadioButton;
+    private JRadioButton fragmentRadioButton;
+    private JTextField loginTextField1;
+    private JButton addMethodButton;
+    private JTextField stringUsernameStringPasswordTextField;
+    private JTextField loginTextField;
+}
